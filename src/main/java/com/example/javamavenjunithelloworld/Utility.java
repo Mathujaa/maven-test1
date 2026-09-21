ca564431292bc1059be03710741e2a4d4a9e41d6
2312026 <2312026@nec.edu.in>
2026-09-21 22:23:45 +0530

    MATHUJAAM0001 - Wrote the code for finding the area of the circle

---
+++ src/main/java/com/example/javamavenjunithelloworld/Utility.java
@@ -0,0 +1,9 @@
+package com.example.javamavenjunithelloworld;
+
+public class Utility{
+public static void mathujaa(){
+double radius=5;
+double area=Math.PI*radius*radius;
+System.out.println("Area of circle: "+area);
+}
+}
